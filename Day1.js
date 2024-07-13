6d59dffe03868c10d4e8e1faf3886a75636ae47d
Harsh Vardhan <114940141+Carnival02@users.noreply.github.com>
2024-07-13 23:30:46 +0530

    Completed Day1.js

--- Day1.js
+++ Day1.js
@@ -1,1 +1,46 @@
+//Task:- 1
+var number=56
+console.log(number);
+
+//Task:- 2
+let str="Harsh";
+console.log(str);
+
+//Task:-3
+
+const isLoogedin="true"
+console.log(isLoogedin);
+
+//Task:-4
+let strr="Harsh"
+let num=45
+const myArra =new Array(1,2,3,4)
+const isSignIn=true
+const user={
+    name:"HArsh",
+    age:18,
+
+}
+
+console.log(typeof(strr));
+console.log(typeof(num));
+console.log(typeof(myArra));
+console.log(typeof(isSignIn));
+console.log(typeof(user));
+
+//Task :- 5
+
+let numbr=45
+console.log(numbr);
+numbr=56
+console.log(numbr);
+
+//Task :- 6
+
+const jk=86
+console.log(jk);
+jk=56
+console.log(jk); // Shows error because we are trying to assign a new value to constant variable
+
+
 
